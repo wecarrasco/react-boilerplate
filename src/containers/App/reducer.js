a0d82efb50eb00cfb06565bb8256efabff9f78e7
Rene Rosa <realpasro09@hotmail.com>
2017-09-20 15:00:44 -0600

    adding redux saga

--- src/containers/App/reducer.js
+++ src/containers/App/reducer.js
@@ -1,15 +1,26 @@
 import type {AppState} from './types';
+import {GET_SAMPLE_DATA_SUCCESS, GET_SAMPLE_DATA_ERROR_MESSAGE} from './constants';
 
 const initialState: AppState = {
   buildInformation: {
     frontEndBuildNumber: process.env.REACT_APP_BUILD_NUMBER,
     deploymentEnvironment: process.env.REACT_APP_DEPLOYMENT_ENV,
   },
+  sampleData: [],
+  sampleErrorMessage: '',
 };
 
 const app = (state: AppState = initialState, action): AppState => {
   const {type} = action;
   switch (type) {
+    case GET_SAMPLE_DATA_SUCCESS:
+    {
+      return {...state, sampleData: action.data};
+    }
+    case GET_SAMPLE_DATA_ERROR_MESSAGE:
+    {
+      return {...state, sampleErrorMessage: action.errorMessage};
+    }
     default:
       return state;
   }
--- src/store.js
+++ src/store.js
@@ -2,13 +2,18 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import {routerMiddleware} from 'react-router-redux';
 import {persistStore, autoRehydrate} from 'redux-persist';
 import createHistory from 'history/createBrowserHistory';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from './rootReducer';
 
 export const history = createHistory();
 
+const sagaMiddleware = createSagaMiddleware();
 const initialState = {};
 const enhancers = [];
-const middleware = [routerMiddleware(history)];
+const middleware = [
+  sagaMiddleware,
+  routerMiddleware(history)
+];
 
 if (process.env.NODE_ENV === 'development') {
   const devToolsExtension = window.devToolsExtension;
@@ -22,5 +27,6 @@ const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers, 
 
 const store = createStore(rootReducer, initialState, composedEnhancers);
 persistStore(store);
+store.runSaga = sagaMiddleware.run;
 
 export default store;
