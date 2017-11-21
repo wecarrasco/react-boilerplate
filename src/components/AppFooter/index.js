7a1c79cf7662a534d54ff04bbf42babffde46e1b
dantedubon <dante.dubon@gmail.com>
2017-11-20 21:30:48 -0600

    testing prettier

--- src/components/AppFooter/index.js
+++ src/components/AppFooter/index.js
@@ -1,18 +1,24 @@
 // @flow
-import type { BuildInformation } from '../../containers/App/types';
-import React from 'react';
-import { Well } from 'react-bootstrap';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import type { BuildInformation } from "../../containers/App/types";
+import React from "react";
+import { Well } from "react-bootstrap";
+import { FormattedMessage } from "react-intl";
+import messages from "./messages";
 
-const AppFooter = ({ deploymentEnvironment, frontEndBuildNumber }: BuildInformation) => (
+const AppFooter = ({
+  deploymentEnvironment,
+  frontEndBuildNumber
+}: BuildInformation) => (
   <Well>
     <p>
-      <FormattedMessage {...messages.FrontEndInformation} values={{ deploymentEnvironment, frontEndBuildNumber }} />
+      <FormattedMessage
+        {...messages.FrontEndInformation}
+        values={{ deploymentEnvironment, frontEndBuildNumber }}
+      />
     </p>
     <p>
-      {' '}
-      <FormattedMessage {...messages.BackendVersion} />{' '}
+      {" "}
+      <FormattedMessage {...messages.BackendVersion} />{" "}
     </p>
   </Well>
 );
