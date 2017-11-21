c34e767bd59bb320320200ebe0481541278b8061
dantedubon <dante.dubon@gmail.com>
2017-11-20 21:29:54 -0600

    added prettier

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
 // @flow
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
-import store, { history } from './store';
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-import App from './containers/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "react-router-redux";
+import store, { history } from "./store";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import App from "./containers/App";
 // $FlowFixMe
-import registerServiceWorker from './registerServiceWorker';
-import appSagas from './containers/App/sagas';
-import LanguageProvider from './containers/LanguageProvider';
-import i18n from './i18n';
+import registerServiceWorker from "./registerServiceWorker";
+import appSagas from "./containers/App/sagas";
+import LanguageProvider from "./containers/LanguageProvider";
+import i18n from "./i18n";
 
-import { addLocaleData } from 'react-intl';
+import { addLocaleData } from "react-intl";
 // $FlowFixMe
-import en from 'react-intl/locale-data/en';
+import en from "react-intl/locale-data/en";
 // $FlowFixMe
-import es from 'react-intl/locale-data/es';
+import es from "react-intl/locale-data/es";
 
 addLocaleData([...en, ...es]);
 
-if (process.env.NODE_ENV === 'production') {
-  require('loggly-jslogger');
+if (process.env.NODE_ENV === "production") {
+  require("loggly-jslogger");
 }
 
 // $FlowFixMe
 appSagas.map(store.runSaga);
-
 ReactDOM.render(
   <Provider store={store}>
     <LanguageProvider messages={i18n.messages}>
@@ -38,6 +37,6 @@ ReactDOM.render(
     </LanguageProvider>
   </Provider>,
   // $FlowFixMe
-  document.getElementById('root')
+  document.getElementById("root")
 );
 registerServiceWorker();
