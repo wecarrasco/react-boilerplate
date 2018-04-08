5822e48beb374437655c324177f7225384f9270d
dante.dubon <dante.dubon@gmail.com>
2018-04-08 15:05:42 -0600

    updating code to new api

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,9 +1,15 @@
-import { combineReducers } from 'redux';
+import { persistCombineReducers } from 'redux-persist';
 import { routerReducer } from 'react-router-redux';
+// $FlowFixMe
+import storage from 'redux-persist/lib/storage';
 import app from './containers/App/reducer';
 import languageProvider from './containers/LanguageProvider/reducer';
+const config = {
+  key: 'primary',
+  storage
+};
 
-export default combineReducers({
+export default persistCombineReducers(config, {
   routing: routerReducer,
   app,
   languageProvider
