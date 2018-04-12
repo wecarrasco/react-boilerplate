a2ee71ec35ddf8ef13a116102033c942cf4be3a8
dante.dubon <dante.dubon@gmail.com>
2018-04-12 16:10:11 -0600

    Title added to Send Message

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -4,11 +4,15 @@ import CheckBox from './CheckBox';
 import Button from './Button';
 import { Card } from './Card';
 import { Container, Col, Row } from './Grid';
+import Title from './Title';
 
 // $FlowFixMe
 const SendMessage = props => (
   <Card>
     <Container>
+      <Row>
+        <Title />
+      </Row>
       <Row>
         <Col>
           <AddMessage />
---
+++ src/components/Title.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+//import Button from './Button';
+
+const InputGroup = styled.div.attrs({
+  className: 'input-group mb-3'
+})``;
+
+const InputPrepend = styled.div.attrs({
+  className: 'input-group-prepend'
+})``;
+
+const InputTitle = styled.span.attrs({
+  className: 'input-group-text'
+})``;
+
+const InputText = styled.input.attrs({
+  className: 'form-control',
+  type: 'text',
+  placeholder: 'Title',
+  'aria-label': 'Title',
+  'aria-describedby': 'basic-addon1'
+})``;
+
+// $FlowFixMe
+const Title = props => (
+  <InputGroup>
+    <InputPrepend>
+      <InputTitle>Title</InputTitle>
+    </InputPrepend>
+    <InputText />
+  </InputGroup>
+);
+
+/** @component */
+export default Title;
